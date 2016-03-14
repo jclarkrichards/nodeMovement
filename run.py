af22ed71a6c54c67beae1225dd1d27ac5e18d745
jclarkrichards <jclarkrichards@gmail.com>
2016-03-14 14:10:45 -0400

    Update run.py

--- run.py
+++ run.py
@@ -7,15 +7,13 @@ TILEWIDTH, TILEHEIGHT = (16, 16)
 SCREENSIZE = (TILEWIDTH*30, TILEHEIGHT*30)
 pygame.init()
 screen = pygame.display.set_mode(SCREENSIZE, 0, 32)
+clock = pygame.time.Clock()
 background = pygame.surface.Surface(SCREENSIZE).convert()
 background.fill((0,0,0))
 
 nodes = NodeGroup(TILEWIDTH, TILEHEIGHT)
 nodes.createNodeList('maze_test.txt')
-
-clock = pygame.time.Clock()
-
-jon = Entity(nodes.nodeDict)
+jon = Entity(nodes.nodeDict, 2)
 
 while True:
     dt = clock.tick(30) / 1000.0
