bf8fbd1fe6f7e74b23808fc60ed36c5f777128c3
jclarkrichards <jclarkrichards@gmail.com>
2016-03-14 09:51:44 -0400

    Update nodemovement3.py

--- nodemovement3.py
+++ nodemovement3.py
@@ -12,7 +12,6 @@ class FourWayContinuous(FourWayAbstract):
             self.setEntityDirection(self.entity.direction)
 
     def update(self, dt):
-        print self.direction
         self.moveTowardsTarget(dt)
         
         if self.overshotTarget():
@@ -20,33 +19,43 @@ class FourWayContinuous(FourWayAbstract):
             #self.portal()
             self.setValidDirections()
 
-            #current direction in the new set of valid directions
             if self.direction:# if a key is being pressed
                 #if pressed direction is in the valid directions
-                if self.direction in self.validDirections:
-                    #if the direction key being pressed is not the same as
-                    #entities current direction
+                if self.isValidDirection(self.direction):
                     if self.direction != self.entity.direction:
-                        self.entity.position = self.nodes[self.node].position
-                        self.setEntityDirection(self.direction)
+                        self.changeDirection()
                     else:#is in the same direction
                         self.setTarget(self.entity.direction)
                 else: #key direction not in valid directions
-                    if self.entity.direction in self.validDirections:
-                        self.setTarget(self.entity.direction)
-                    else:
-                        self.entity.position = self.nodes[self.node].position
-                        self.entity.direction = STOP
+                    self.checkCurrentDirection()
             else: #key not being pressed
-                self.entity.position = self.nodes[self.node].position
-                self.entity.direction = STOP
-        else: #direction key not being pressed
+                self.restOnNode()
+               
+        else: #has not overshot target
             if self.entity.direction == STOP:
-                if self.direction in self.validDirections:
+                if self.isValidDirection(self.direction):
                     self.setEntityDirection(self.direction)
             else:
                 if self.direction == self.entity.direction*-1:
                     self.reverseDirection()
+                    
+    def checkCurrentDirection(self):
+        '''Check if entity is able to continue in current direction'''
+        if self.isValidDirection(self.entity.direction):
+            self.setTarget(self.entity.direction)
+        else:
+            self.restOnNode()
 
+    def restOnNode(self):
+        self.entity.position = self.nodes[self.node].position
+        self.entity.direction = STOP
+        
+    def isValidDirection(self, direction):
+        return direction in self.validDirections
+        
+    def changeDirection(self):
+        self.entity.position = self.nodes[self.node].position
+        self.setEntityDirection(self.direction)
+        
     def keyDiscrete(self, key):
         pass
