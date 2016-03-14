5f132ebf037a2d01adb69146a4d302f53f65acb9
jclarkrichards <jclarkrichards@gmail.com>
2016-03-14 14:48:58 -0400

    Update nodehandler.py

--- nodehandler.py
+++ nodehandler.py
@@ -1,1 +1,18 @@
+from node import NodeGroup
+from nodemovement import FourWayMovement
+
+class MapNode(object):
+    def __init__(self):
+        self.mapName = ''
+        self.nodes = None
+        self.neighbors = {}
+        self.ID = 0
+        self.playerStart = 0
+        
+
 class NodeHandler(object):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        
+        
