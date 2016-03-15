17c0ccfa8b1b5d199a7e61e2c9e14fc4a6cb9337
jclarkrichards <jclarkrichards@gmail.com>
2016-03-14 22:41:38 -0400

    updated more

--- entity.py
+++ entity.py
@@ -1,20 +1,15 @@
 import pygame
-#from nodemovement import FourWayMovement
-#from nodemovement2 import FourWayDiscrete
-#from nodemovement3 import FourWayContinuous
 from vectors import Vector2D
 
 class Entity(object):
-    def __init__(self):#, nodes, startNode):
+    def __init__(self):
         self.ID = 0
-        self.position = Vector2D() #nodes[startNode].position
+        self.position = Vector2D() 
         self.direction = 0
-        #self.move = FourWayMovement(nodes, startNode, self, version=3)
-        self.speed = 100
+        self.speed = 60
         
     def update(self, dt):
         pass
-        #self.move.update(dt)
 
     def render(self, screen):
         x, y = self.position.toTuple()
--- run.py
+++ run.py
@@ -13,22 +13,18 @@ background = pygame.surface.Surface(SCREENSIZE).convert()
 background.fill((0,0,0))
 
 nodes = NodeHandler(TILEWIDTH, TILEHEIGHT)
-#nodes = NodeGroup(TILEWIDTH, TILEHEIGHT)
-#nodes.createNodeList('maze_test.txt')
-jon = Entity() #nodes.nodeDict, 2)
+jon = Entity()
 nodes.setPlayer(jon)
 
 while True:
     dt = clock.tick(30) / 1000.0
     key_pressed = pygame.key.get_pressed()
-    #jon.move.setKeyPressed(key_pressed)
     nodes.area.movers[jon.ID].setKeyDirection(key_pressed)
     
     for event in pygame.event.get():
         if event.type == QUIT:
             exit()
  
-    #jon.update(dt)
     nodes.update(dt)
     screen.blit(background, (0,0))
     nodes.render(screen)
