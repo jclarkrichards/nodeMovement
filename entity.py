ae8127549c177c263161bb0045b7c7ac1ca1d4bc
jclarkrichards <jclarkrichards@gmail.com>
2016-03-16 10:05:45 -0400

    Update entity.py

--- entity.py
+++ entity.py
@@ -1,15 +1,21 @@
 import pygame
 from vectors import Vector2D
+from nodemovement import FourWayMovement
 
 class Entity(object):
-    def __init__(self):
+    def __init__(self, nodes, nodeVal):
         self.ID = 0
         self.position = Vector2D() 
         self.direction = 0
         self.speed = 60
+        self.mover = FourWayMovement(nodes, nodeVal, self, version=3)
         
     def update(self, dt):
         pass
+    
+    def move(self, dt):
+        '''Move entity around the nodes'''
+        self.mover.update(dt)
 
     def render(self, screen):
         x, y = self.position.toTuple()
