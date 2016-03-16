c32061702e6a6fa863ae8efcb7dc0635522a4a2c
jclarkrichards <jclarkrichards@gmail.com>
2016-03-16 10:43:48 -0400

    Update world.py

--- world.py
+++ world.py
@@ -1,3 +1,74 @@
+import pygame
+from pygame.locals import *
+
 class World(object):
-    def __init__(self, width, height, multiplier):
+    def __init__(self):
+        self.screenSize = (0, 0)
+        self.tileSize = 1
+        self.background = None
+        self.screen = None
+        self.mapName = ''
+        self.nodes = {}
+        self.ID = 0
+        self.mapNeighbors = {}
+        self.keyPressed = 0
+        self.dynamicOBJ = {}
+        
+    def setup(self, x, y, tileSize=1):
+        '''Set the width and height and size of each tile'''
+        self.tileSize = tileSize
+        pygame.init()
+        self.screen = self.setScreenSize(x*tileSize, y*tileSize)
+        self.setBackground()
+        
+    def setScreenSize(self, width, height):
+        self.screenSize = (width, height)
+        return pygame.display.set_mode(self.screenSize, 0, 32)
+        
+    def setBackground(self):
+        self.background = pygame.surface.Surface(self.screenSize).convert()
+        self.background.fill((0,0,0))
+        
+    def addNodes(self):
         pass
+    
+    def removeNodes(self):
+        pass
+    
+    def __addObject__(self, database, obj):
+        if obj.ID in database.keys():
+            return
+        if len(database) == 0:
+            obj.setID(0)
+            database[0] = obj
+            return
+        newID = max(database.keys()) + 1
+        obj.setID(newID)
+        database[newID] = obj
+    
+    def addDynamicObject(self, obj):
+        self.__addObject__(self.dynamicOBJ, obj)
+    
+    def handleEvents(self):
+        '''Checks for key presses, mouse clicks, etc...'''
+        self.keyPressed = pygame.key.get_pressed()
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                exit()
+    
+    def update(self, dt):
+        pass
+    
+    def clearDynamicObjects(self):
+        self.dynamicOBJ = {}
+        
+    def clearAll(self):
+        '''Clear out all objects and nodes from the world'''
+        self.nodes = {}
+        self.clearDynamicObjects()
+    
+    def render(self):
+        self.screen.blit(self.background, (0,0))
+        #draw nodes for testing purposes
+        for item in self.dynamicOBJ.values():
+            item.render(self.screen)
