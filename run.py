772e7883fe979ee3623b35666716a40b7df2ec60
jclarkrichards <jclarkrichards@gmail.com>
2016-03-16 10:49:39 -0400

    Update run.py

--- run.py
+++ run.py
@@ -3,31 +3,24 @@ from pygame.locals import *
 from node import NodeGroup
 from entity import Entity
 from nodehandler import NodeHandler
+from world import World
 
-TILEWIDTH, TILEHEIGHT = (16, 16)
-SCREENSIZE = (TILEWIDTH*25, TILEHEIGHT*25)
-pygame.init()
-screen = pygame.display.set_mode(SCREENSIZE, 0, 32)
+world = World()
+world.setup(25, 25, 16)
+
 clock = pygame.time.Clock()
-background = pygame.surface.Surface(SCREENSIZE).convert()
-background.fill((0,0,0))
-
 nodes = NodeHandler(TILEWIDTH, TILEHEIGHT)
 player = Entity()
-nodes.setPlayer(player)
+#nodes.setPlayer(player)
+world.addDynamicObject(player, setAsPlayer=True)
 
 while True:
+    world.handleEvents()
     dt = clock.tick(30) / 1000.0
-    key_pressed = pygame.key.get_pressed()
-    player.move(dt, key_pressed)
-    
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            exit()
- 
-    nodes.update(dt)
-    screen.blit(background, (0,0))
-    nodes.render(screen)
-    player.render(screen)
-
+    #key_pressed = pygame.key.get_pressed()
+    #player.move(dt, key_pressed)
+    world.update(dt)
+    #nodes.update(dt)
+    #nodes.render(screen)
+    world.render()
     pygame.display.update()
