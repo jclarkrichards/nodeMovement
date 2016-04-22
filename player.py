8debb5c4bd50a0836e079c087695f0f46bf10167
jclarkrichards <jclarkrichards@gmail.com>
2016-04-22 09:57:40 -0400

    Update player.py

--- player.py
+++ player.py
@@ -1,6 +1,5 @@
 import pygame
 from vectors import Vector2D
-#from nodemovement import FourWayMovement
 from entity import Entity
 
 class Player(Entity):
@@ -10,7 +9,6 @@ class Player(Entity):
         self.previousDirection = 0
         self.facingDirection = 0
         self.speed = 60
-        #self.mover = FourWayMovement(self, version=3)
         self.velocity = Vector2D()
         self.overrideKeys = False
         self.npc = False
@@ -18,16 +16,6 @@ class Player(Entity):
     def update(self, dt):
         pass
     
-    #def loadNewNodes(self, nodes, startNode):
-    #    '''Allow entity to move around the new node set'''
-    #    self.mover.loadNewNodes(nodes, startNode)
-    #    self.mover.test()
-        
-    #def move(self, dt, key_pressed=None):
-    #    '''Move entity around the nodes'''
-    #    if key_pressed and not self.overrideKeys:
-    #        self.mover.setKeyDirection(key_pressed)
-    #    self.mover.update(dt)
-    #    self.overrideKeys = False
+ 
 
 
